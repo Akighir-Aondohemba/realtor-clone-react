cb701d7a53f3bcd630b543e40d847b8ea75ab4ee
Aondohemba <akighirjoseph77@gmail.com>
2023-08-05 00:08:46 +0100

    create sign in, sign up and forgot password pages and Oauth component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,6 +5,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('@tailwindcss/forms')],
 }
+
+
 
