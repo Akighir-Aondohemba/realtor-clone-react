e17eb774f1772f86cbc0f661e4be06e2bb5945aa
Aondohemba <akighirjoseph77@gmail.com>
2023-08-24 13:24:00 +0100

    complete the header compnent and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,72 @@
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Header() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    function pathMathRoute(route){
-        if(route === location.pathname)
-        return true;
-    }
+  const [pageState, setPageState] = useState("Sign in");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    });
+
+  }, [auth]);
+
+  function pathMatchRoute(route) {
+    if(route === location.pathname)
+    return true;
+  }
+
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
-     <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
-            <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" 
-            alt="logo" className='h-5 cursor-pointer' onClick={()=>navigate("/")}/>         
-            </div>
-            <div>
-                <ul className='flex space-x-10'>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
-                </ul>
-            </div>
-     </header>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className='h-5 cursor-pointer'
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className='flex space-x-10'>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMatchRoute("/") && "text-black border-b-[3px] border-b-red-500"
+            
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                pathMatchRoute("/offers") && "text-black border-b-[3px] border-b-red-500"
+                
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                 "text-black border-b-[3px] border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+              {pageState}
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
